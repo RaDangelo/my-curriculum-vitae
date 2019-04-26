943eb69db0c6ba58eceeb29b855a78ac340f1ed9
Rafael Bergamini <rafaelbergamini@Rafaels-MacBook-Pro-2.local>
2019-04-26 00:34:43 -0300

    ajuste curriculum

--- src/component/App.js
+++ src/component/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import reactLogo from './logos/react-logo.svg';
-import angularLogo from './logos/angular-logo.svg';
-import nodeLogo from './logos/node-logo.svg';
-import jsLogo from './logos/js-logo.svg';
-import javaLogo from './logos/java-logo.svg';
-import htmlLogo from './logos/html-logo.svg';
-import cssLogo from './logos/css-logo.svg';
+import reactLogo from '../logos/react-logo.svg';
+import angularLogo from '../logos/angular-logo.svg';
+import nodeLogo from '../logos/node-logo.svg';
+import jsLogo from '../logos/js-logo.svg';
+import javaLogo from '../logos/java-logo.svg';
+import htmlLogo from '../logos/html-logo.svg';
+import cssLogo from '../logos/css-logo.svg';
 
 import './App.css';
+import './bird.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" onKeyPress={(e) => this.jump(e)} >
         <div className="App-header">
           <div className="logosContainer">
             <span className="arrowChange">{"<"}</span>
@@ -27,12 +28,28 @@ class App extends Component {
             {/* <img src={javaLogo} className="App-logo Lateral-logo small" alt="iib-logo" title="Integration Bus" /> */}
             <span className="arrowChange">{">"}</span>
           </div>
-          <h2><code className="name">{"<curriculum>"} Rafael D'Angelo Bergamini {"</curriculum>"}</code></h2>
+          {/* <h2><code className="name">{"<curriculum>"} Rafael D'Angelo Bergamini {"</curriculum>"}</code></h2> */}
         </div>
         <p className="App-intro">
         </p>
+        <div id="bird" className="bird-container top">
+          <div className="bird bird-one"></div>
+        </div>
+        <div className="bird-container bottom">
+          <div className="bird bird-two"></div>
+        </div>
       </div>
     );
+  }
+
+  jump(event) {
+    console.log(event.key);
+    if (event.key === "Space") {
+      document.getElementById('bird').className = 'bird-container top jump';
+      setTimeout(() => {
+        document.getElementById('bird').className = 'bird-container top';
+      }, 1000);
+    }
   }
 }
 
